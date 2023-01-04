8f047668ad7e24c81fcedd959db5eeed07e88260
sjf <[sujunfu1997@gmail.com]>
2023-01-04 11:11:15 +0800

    新建模块

---
+++ junfukt-provider/src/main/java/junfukt/provider/ProviderApplication.java
@@ -0,0 +1,21 @@
+package junfukt.provider;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author admin
+ */
+@Slf4j
+@SpringBootApplication
+public class ProviderApplication {
+    public static void main(final String[] args) {
+        SpringApplication.run(ProviderApplication.class, args);
+//        new SpringApplicationBuilder(ProviderApplication.class)
+//                .web(WebApplicationType.NONE) // 非 Web 应用
+//                .run(args);
+        log.info("========== provider is success ==========");
+    }
+
+}
