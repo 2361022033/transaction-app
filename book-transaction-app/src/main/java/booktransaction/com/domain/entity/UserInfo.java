868e4139669d8e186978160fa4ef1b2e6c0d7d13
sjf <[sujunfu1997@gmail.com]>
2023-01-31 14:05:12 +0800

    新建用户类

--- book-transaction-app/src/main/java/booktransaction/com/domain/entity/UserInfo.java
+++ book-transaction-app/src/main/java/booktransaction/com/domain/entity/UserInfo.java
@@ -3,47 +3,80 @@ package booktransaction.com.domain.entity;
 import lombok.Data;
 import lombok.experimental.Accessors;
 
+import javax.persistence.Id;
 import javax.persistence.Table;
-import java.io.Serializable;
 import java.util.Date;
 
 @Data
 @Accessors(chain = true)
-@Table(name = "junfukt_user")
-public class User implements Serializable {
-    private static final long serialVersionUID = 1L;
+@Table(name = "user_info")
+public class UserInfo {
     /**
-     * id
+     * PK
      */
+    @Id
     private Long id;
+
     /**
-     * 用户id
+     * 用户名
      */
-    private String userId;
+    private String userName;
+
     /**
-     * 工号
+     * 密码
      */
-    private String staffNo;
+    private String password;
+
     /**
      * 真实姓名
      */
     private String realName;
+
+    /**
+     * 性别
+     */
+    private Long sex;
+
     /**
      * 昵称
      */
     private String nickName;
+
     /**
      * 头像
      */
     private String avatar;
+
     /**
      * 个性签名
      */
     private String introduction;
+
+    /**
+     * 账户余额
+     */
+    private Long balance;
+
+    /**
+     * 已买数量
+     */
+    private Long bugNumber;
+
+    /**
+     * 已售数量
+     */
+    private Long soldOutNumber;
+
+    /**
+     * 在售数量
+     */
+    private Long onSaleNumber;
+
     /**
      * 创建时间
      */
     private Date createTime;
+
     /**
      * 修改时间
      */
