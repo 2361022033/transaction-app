2adbe6d21f14e458c34fb1392ec434ae68f9e718
sjf <[sujunfu1997@gmail.com]>
2023-01-31 13:39:38 +0800

    图书接口完善

--- DB/first.sql
+++ DB/first.sql
@@ -1,20 +1,3 @@
--- CREATE TABLE junfukt_user
--- (
---     id bigint unsigned NOT NULL AUTO_INCREMENT COMMENT 'PK',
---     user_id VARCHAR(32) COLLATE utf8mb4_general_ci NOT NULL COMMENT '用户id',
---     staff_no VARCHAR(32) COLLATE utf8mb4_general_ci NOT NULL COMMENT '工号',
---     real_name VARCHAR(32) COLLATE utf8mb4_general_ci NOT NULL COMMENT '真实姓名',
---     nick_name VARCHAR(32) COLLATE utf8mb4_general_ci COMMENT '昵称',
---     avatar VARCHAR(256) COLLATE utf8mb4_general_ci COMMENT '头像',
---     introduction VARCHAR(128) COLLATE utf8mb4_general_ci COMMENT '个性签名',
---     create_time DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
---     update_time DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP  COMMENT  '修改时间',
---     PRIMARY KEY (id),
---     CONSTRAINT uk_user_id UNIQUE (user_id)
--- )
---     ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='用户';
-
-
 -- 新建数据库
 create
 database book_transaction;
